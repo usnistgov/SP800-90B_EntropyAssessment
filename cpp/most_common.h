10c6579b75eebd4949fba85809c90dac9112b1e1
celic <cceli@codequarry.net>
2016-07-25 11:07:53 -0400

    Adds most common value function for an entropy estimate

---
+++ cpp/most_common.h
@@ -0,0 +1,25 @@
+#include "utils.h"
+
+double most_common(byte data[]){
+
+	map<byte, int> p;
+	map_init(p);
+
+	for(int i = 0; i < SIZE; i++){
+		p[data[i]]++;
+	}
+
+	int mode = 0;
+	map<byte, int>::iterator itr;
+	for(itr = p.begin(); itr != p.end(); ++itr){
+		if(itr->second > mode){
+			mode = itr->second;
+		}
+	}
+
+	double pmax = mode / (double)SIZE;
+	double ubound = pmax + 2.576 * sqrt(pmax * (1.0 - pmax) / (double)SIZE);
+	double pu = min(1.0, ubound);
+
+	return -log2(pu);
+}
